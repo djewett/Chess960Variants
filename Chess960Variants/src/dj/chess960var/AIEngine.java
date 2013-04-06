601c9d53072669839095db817ce644b60a924ef3
Damian Jewett <damian.jewett@gmail.com>
2013-04-06 19:19:04 -0300

    starting to add evaluation function(s)

--- Chess960Variants/src/dj/chess960var/AIEngine.java
+++ Chess960Variants/src/dj/chess960var/AIEngine.java
@@ -22,8 +22,8 @@ public class AIEngine
 			for( int j = 0; j < BOARD_REP[0].length; j++ )
 			{
 				// Create a list of possible moves in next two loops and sort
-				// them according to some evaluation function and only keep the
-				// top 10 or so:
+				// them according to some evaluation function and only keep
+				// the top 10 or so:
 				
 				pc piece = BOARD_REP[i][j];
 				
@@ -36,7 +36,7 @@ public class AIEngine
 						for( int newJ = 0; newJ < BOARD_REP.length; newJ++ )
 						{
 							if( (i != newI || j != newJ)  &&
-								gameModel.isValidMove( i, j, newI, newJ, false ) )
+								gameModel.isValidMove(i,j,newI,newJ,false) )
 							{
 								int[] move = new int[4];
 								move[0] = i;
@@ -52,11 +52,62 @@ public class AIEngine
 			}
 		}
 		
-		return convertMoveToString( possibleMoves.get(0) );
+		ArrayList<int[]> decreasedPossibleMoves = 
+			trimPossibleMoves( possibleMoves );
+		
+		int[] theMoveToMake = evaluateMoves( decreasedPossibleMoves );
+		
+		return convertMoveToString( theMoveToMake );
+	}
+	
+	private static ArrayList<int[]> trimPossibleMoves( 
+		ArrayList<int[]> possibleMoves )
+	{
+		// This function should take in a list of possible moves and trim 
+		// away some of the obviously bad moves (as well as some of the less 
+		// obviously bad ones) and return a new list of potential moves that 
+		// is as small as possible.
+		
+		// TODO: Need to run some kind of evaluations to rank these moves 
+		// and trim off the bottom x moves (need to figure out what x
+		// should be as well)
+		// 
+		//Trim off first move, just for testing:
+		// Consider using evaluatePosition() function below
+		possibleMoves.remove(0);
+		return possibleMoves;
+	}
+	
+	private static int[] evaluateMoves( ArrayList<int[]> moves )
+	{
+		// This function should take in a list of moves, evaluate all of them, 
+		// and return the best one, according to the evaluation function.
+		// This is the "imperfect information" step.
+		
+		// TODO: Run evaluations instead of simply always returning the first 
+		// move:
+		// Consider using evaluatePosition() function below
+		return moves.get(0);
+	}
+	
+	private static int evaluatePosition( final pc[][] board_rep, 
+										 boolean isLightsTurn )
+	{
+		// Given a representation of the board, where pieces are on the
+		// board and whose move it is, this function generates a "utility"
+		// for that state in the game.  This utility should be a measure of
+		// how good the given position is for whoever's turn it is.  It
+		// can be positive or negative.
+		
+		// TODO
+		return 0;
 	}
 	
 	private static String convertMoveToString( int[] theMove )
 	{
+		// This is a simple function that converts a 2x2 array representation
+		// of a move to a String representation (for output).
+		
 		// TODO: Add an assertion/pre-condition that theMove is 2x2.
 		
 		String fromSquareChar = convertNumberToCharacter( theMove[1] );
@@ -71,6 +122,9 @@ public class AIEngine
 	
 	private static String convertNumberToCharacter( int num )
 	{
+		// This is a simple function that converts from a column number to a 
+		// column letter (for output).
+		
 		String theChar = "";
 		
 		switch( num )
